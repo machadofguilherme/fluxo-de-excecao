1cf1927e21dca5e56e9cdcb98d5a876b2f6ff6ca
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-17 14:55:07 -0300

    Adicionando recurso para detectar e-mails válidos.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,37 @@ const inputPassword = document.getElementById("input-password");
 const btnLogin = document.getElementById("btn");
 const message = document.getElementById("message");
 
-const checkError = (inputEmail, inputPassword) => {
-  
-  if (
-    !Boolean(inputEmail.value) ||
-    !Boolean(inputPassword.value)
-    ) {
+const emptyInput = (inputEmail, inputPassword) => {
+  if (!Boolean(inputEmail.value) || !Boolean(inputPassword.value)) {
     throw Error("O campo e-mail ou o campo senha estão vazios.");
   }
+}
 
-  if (
-    !Boolean(inputPassword.value.trim()) ||
-    !Boolean(inputEmail.value.trim())
-  ) {
+const spaceInput = (inputPassword) => {
+  if (!Boolean(inputPassword.value.trim()) || !Boolean(inputEmail.value.trim())) {
     throw Error("Este campo não pode conter espaços.");
   }
+}
 
+const lengthInput = (inputPassword) => {
   if (inputPassword.value.length > 8) {
     throw Error("Digite uma senha com no máximo 8 caracteres.");
   }
+}
 
+const containAt = (inputEmail) => {
+  if (inputEmail.value.indexOf('@') > 3) {
+    throw Error("Digite um e-mail válido. Seu nome de usuário deve conter mais que 3 caracteres.");
+  }
+}
+
+const checkError = (inputEmail, inputPassword) => {
+  
+  emptyInput(inputEmail, inputPassword);
+  spaceInput(inputEmail, inputPassword);
+  lengthInput(inputPassword);
+  containAt(inputEmail);
+  
 };
 
 btnLogin.addEventListener("click", (e) => {
@@ -31,10 +42,13 @@ btnLogin.addEventListener("click", (e) => {
   try {
     checkError(inputEmail, inputPassword);
     message.innerHTML = `${inputEmail.value} ${inputPassword.value}`;
+
   } catch (error) {
     message.innerHTML = `${error.message}`;
+
   } finally {
     (inputEmail.value = ""), (inputPassword.value = "");
+
   }
 
 });
